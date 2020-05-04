eb8395dc5f07b9986b6b48815ce7ca809ae86197
Stefanos Veisakis <csd3559@csd.uoc.gr>
2020-05-04 20:04:35 +0300

    init commit

---
+++ Pages/Navigator/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Home from '../HomePage'
+import User from '../User'
+
+
+const Stack = createStackNavigator();
+
+const MyStack = () => {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+          />
+          <Stack.Screen 
+            name="User" 
+            component={User} 
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
+  export default MyStack;
---
+++ Pages/User/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const User = () => {
+
+    return (
+        <View>
+            <Button
+                title="Click Me"
+            />
+        </View>
+    );
+}
+
+export default User
