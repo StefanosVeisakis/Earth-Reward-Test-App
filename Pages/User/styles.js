4ad934b63b8c8eb43146eb933da4b6b716460687
Stefanos Veisakis <csd3559@csd.uoc.gr>
2020-05-06 13:56:18 +0300

    User Page functionality

---
+++ Pages/User/styles.js
@@ -0,0 +1,17 @@
+import {
+    StyleSheet,
+} from 'react-native';
+
+import {
+    Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+const styles = StyleSheet.create({
+    body: {
+        backgroundColor: Colors.white,
+        alignItems: 'center',
+        flex: 1,
+    }
+});
+
+export default styles;
