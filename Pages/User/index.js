86b69944943ea7b8fa93a32a65b0b044b449efc4
Stefanos Veisakis <csd3559@csd.uoc.gr>
2020-05-13 19:14:10 +0300

    config web3 - use of balance0f function
    # Conflicts:
    #	index.js
    #	metro.config.js
    #	package-lock.json
    #	package.json

--- Pages/User/index.js
+++ Pages/User/index.js
@@ -1,28 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import { View, Button, Text } from 'react-native';
 import styles from './styles';
+import Web3 from 'web3';
+import abi from '../../json/abi.json';
 
-const Web3 = require("web3");
-// const web3 = new Web3('https://kovan.infura.io/v3/505763a0a40d4e46b9a611b8f5877672');
+const tokenAddress = "0x8EB6D2CE2Db73Fb058dd9e7CB7961F67c42fEfa1";
+const walletAddress = "0xb984cd9fea5bbb4b8a645af9859d27dab9b3b19c";
+
+const web3 = new Web3(new Web3.providers.HttpProvider(`https://kovan.infura.io/v3/505763a0a40d4e46b9a611b8f5877672`));
+const contract = new web3.eth.Contract(abi, tokenAddress);
 
 const User = () => {
     const [number, setNumber] = useState(0);
 
-    useEffect(() => {
-    }, [])
-
+    const getBalance = () => {
+        const data = contract.methods.balanceOf(walletAddress).call();
+        data.then(setNumber);
+    }
 
     return (
         <View style={styles.body}>
-            <Button
-                title="Show Balance"
-                onPress={() => setNumber(number + 1)}
-            />
-            <Text
-                label="Your Balance:"
-                value={number.count}>
+            <Text style={styles.text}
+                label="Your Balance:">
+                Your Balance:
+            </Text>
+            <Text style={styles.number}
+                label="Balance Number">
                 {number}
             </Text>
+            <Button
+                title="Show Balance"
+                onPress={() => getBalance()}
+            />
         </View>
     );
 }
--- Pages/User/styles.js
+++ Pages/User/styles.js
@@ -11,7 +11,19 @@ const styles = StyleSheet.create({
         backgroundColor: Colors.white,
         alignItems: 'center',
         flex: 1,
-    }
+        justifyContent: 'center',
+    },
+    number: {
+        borderWidth: 1,
+        borderColor: '#000',
+        fontSize: 16,
+        padding: 30,
+        margin: 10,
+        marginBottom: 20,
+    },
+    text: {
+        fontSize: 20,
+    },
 });
 
 export default styles;
